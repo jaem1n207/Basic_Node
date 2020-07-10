9e15d2c79afcad35f815ba0356b37ec170a5094b
jaem1n207 <jaem1n207@naver.com>
2020-07-10 12:16:26 +0900

    [ADD] Connect DB

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+/* DB 연결 */
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://jaemin:dlwoals0307@basicnode.zawmr.mongodb.net/basicnode?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
