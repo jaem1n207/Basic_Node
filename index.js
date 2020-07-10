155258d6def114aee7ab66adfcb393605851eb86
jaem1n207 <jaem1n207@naver.com>
2020-07-10 14:57:08 +0900

    [ADD] BodyParser

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const bodyParser = require("body-parser");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 
 /* DB 연결 */
 const mongoose = require("mongoose");
@@ -18,5 +26,17 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입에 필요한 정보들을 client에서 가져와 DB에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
